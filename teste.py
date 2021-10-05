128122b30c06e1d474a6f5371c5b6329d12e756d
PHMarques14 <83430925+PHMarques14@users.noreply.github.com>
2021-10-05 16:46:12 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -6,31 +6,30 @@ def telaSejaBV():
     tela['bg'] = "#cfcbcb"
     tela.title("IterMobi")
 
-    mainLbl = Label(tela, text="SEJA BEM-VINDO!", font="Beirut 28", bg = "#cfcbcb", fg = "#30343F")
-    mainLbl.place(relx = 0.06,rely=0.03)
+    mainLbl = Label(tela, text="Seja bem-vindo!", font="Beirut 28", bg = "#cfcbcb", fg = "#30343F")
+    mainLbl.place(relx = 0.02,rely=0.03)
     lblBusca = Label(tela, bg = "#00226d")
     lblBusca.place(width=400,height=66,relx = 0, rely= 0.12)
     entryBusca = Entry(lblBusca, relief = FLAT, font= "Helvetica 14")
-    entryBusca.place(width=300,height=50,relx = 0.01, rely= 0.1)
+    entryBusca.place(width=309,height=54,relx = 0.01, rely= 0.05)
 
     img = PhotoImage(file="iconeMic.png")
 
-    lblMic = Label(tela, image=img, bg="#cfcbcb")
-    lblMic.place(x=310,y=85)
+    lblMic = Label(tela, image=img)
+    lblMic.place(x=315,y=86)
 
+    lblLugRec = Label(tela, text="Lugares recentes:", font="Beirut 18", bg = "#cfcbcb", fg = "#5E5E5E")
+    lblLugRec.place(relx = 0.02, rely=0.34)
+    
+    lblSelec = Label(tela)
+    lblSelec.place(width=400,height=240,relx = 0, rely=0.4)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    lblPLugar = Label(lblSelec, text="Graal Resende", font="Beirut 18", bg = "#2955b6", fg = "white", anchor= "nw")
+    lblPLugar.place(width=400,height=80,x= -2, y = -2)
+    lblSLugar = Label(lblSelec, text="Rodoviária Velha", font="Beirut 18", bg = "#160b7d", fg = "white", anchor= "nw")
+    lblSLugar.place(width=400,height=80,x= -2, y = 78)
+    lblTLugar = Label(lblSelec, text="Av. Perimetral Norte, 609", font="Beirut 18", bg = "#6585cd", fg = "white", anchor= "nw")
+    lblTLugar.place(width=400,height=80,x= -2, y = 158)
 
 
 
