2ad97e8e5012d8fa6b1ee1dca1f8da56ef1b8f1e
PHMarques14 <83430925+PHMarques14@users.noreply.github.com>
2021-10-06 16:13:39 -0300

    nova tela adicionada

--- teste.py
+++ teste.py
@@ -36,5 +36,37 @@ def telaSejaBV(self):
 
     self.lblTLugar = Label(self.lblSelec, text="Av. Perimetral Norte, 609", font="Beirut 18", bg = "#6585cd", fg = "white", anchor= "nw")
     self.lblTLugar.place(width=400,height=80,x= -2, y = 158)
-    self.lblPLugarEnder = Label(self.lblSelec, text="Av. Perimetral Norte, 609, Cidade Alegria", font="Beirut 12", bg = "#6585cd", fg = "#bababa", anchor= "w")
-    self.lblPLugarEnder.place(x= -2, y = 200)
+    self.lblTLugarEnder = Label(self.lblSelec, text="Av. Perimetral Norte, 609, Cidade Alegria", font="Beirut 12", bg = "#6585cd", fg = "#bababa", anchor= "w")
+    self.lblTLugarEnder.place(x= -2, y = 200)
+    
+def telaBusca():
+    self.tela = Tk()
+    self.tela.geometry("375x677")
+    self.tela['bg'] = "#cfcbcb"
+    self.tela.title("IterMobi")
+
+    self.mainLbl = Label(self.tela, text="Busca", font="Beirut 28 bold", bg = "#6585cd", fg = "white")
+    self.mainLbl.place(width=380,height=82,relx = 0,rely=0)
+    self.lblBusca = Label(self.tela, bg = "#00226d")
+    self.lblBusca.place(width=400,height=66,relx = 0, rely= 0.12)
+    self.entryBusca = Entry(self.lblBusca, relief = FLAT, font= "Helvetica 14")
+    self.entryBusca.place(width=309,height=54,relx = 0.01, rely= 0.05)
+
+    self.img = PhotoImage(file="iconeMic.png")
+    self.img2 = PhotoImage(file="iconeSeta.png")
+
+    self.lblSeta = Label(self.tela, image=self.img2, bg = "#6585cd")
+    self.lblSeta.place(x=5,y=20)
+
+    self.lblMic = Label(self.tela, image=self.img)
+    self.lblMic.place(x=315,y=86)
+
+    self.lblPLugar = Label(self.tela, text="Graal Resende", font="Beirut 18", bg = "#ededed", fg = "black", anchor= "nw")
+    self.lblPLugar.place(width=400,height=80,x= -2, y = 145)
+    self.lblPLugarEnder = Label(self.tela, text="Rodovia Presidente Dutra, Paraíso", font="Beirut 12", bg = "#ededed", fg = "#30343F", anchor= "w")
+    self.lblPLugarEnder.place(x= -2, y = 185)
+
+    self.lblSLugar = Label(self.tela, text="Graal Embaixador", font="Beirut 18", bg = "#ededed", fg = "black", anchor= "nw")
+    self.lblSLugar.place(width=400,height=80,x= -2, y = 227)
+    self.lblSLugarEnder = Label(self.tela, text="Rod. Pres. Dutra, Fazenda da Barra", font="Beirut 12", bg = "#ededed", fg = "#30343F", anchor= "w")
+    self.lblSLugarEnder.place(x= -2, y = 267)
